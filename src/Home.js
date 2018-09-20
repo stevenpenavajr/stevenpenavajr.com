d791c4c7eaabb88d6cf26331554535de95cfd71d
Steven Penava <steven.penava@gmail.com>
2018-09-19 23:58:21 -0400

    Added social network logos, changed font, added particles in background.

--- src/Home.js
+++ src/Home.js
@@ -5,6 +5,10 @@ import './styles/Home.css';
 import Particles from 'react-particles-js';
 import jsonData from './data/particlesjs-config.json';
 
+import githubLogo from './assets/github-logo.svg'
+import linkedInLogo from './assets/linkedin-logo-round.svg'
+import mailLogo from './assets/at.svg'
+
 const particleOptions = jsonData;
 
 
@@ -14,10 +18,22 @@ class Home extends Component {
 		return (
 			<div>
 				<Particles params = {particleOptions} className="Particle-background"/>
-				<div className="Title-container">
-					<h1 id="Name">Steven Penava</h1>
+				<div className="title-container">
+					<h1 id="title-name">STEVEN PENAVA</h1>
+					<div id="title-button-container">
+						<button type="button" class="btn btn-outline-primary title-button">EDUCATION</button>
+						<button type="button" class="btn btn-outline-primary title-button">FIELD EXPERIENCE</button>
+						<button type="button" class="btn btn-outline-primary title-button">SKILL SET</button>
+						<button type="button" class="btn btn-outline-primary title-button">CURRENT PROJECTS</button>
+					</div>
+					<div id="title-img-container">
+						<a href="https://github.com/stevenpenavajr" target="_blank"><img className="title-logo" src={githubLogo} alt="Github"></img></a>
+						<a href="https://www.linkedin.com/in/steven-penava-jr-a58398bb/" target="_blank"><img className="title-logo" src={linkedInLogo} alt="LinkedIn"></img></a>
+						<a href="mailto:steven.penava@gmail.com"><img className="title-logo" src={mailLogo} alt="E-mail"></img></a>
+					</div>
 				</div>
 			</div>
+
 		);
 	}
 }
